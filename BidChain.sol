5dbc9f971a8b49f523e516085f588a539b3198ab
Avil Avate <avilavate123@gmail.com>
2018-06-02 21:46:13 +0530

    Added bidding functionality [initiate bid, flagging user who cant bid, changing item owner].

--- BidChain.sol
+++ BidChain.sol
@@ -4,10 +4,30 @@ import "./contracts.sol";
 
 contract BidChain is AuctionHouse {
     
+    struct biddingData {
+        uint endTime;
+        uint highestBid;
+        address bidder;
+        bool initialized;
+        mapping (address => bool) flaggedUsers;
+    }
+    
+    mapping (uint => biddingData) itemToBiddingMap;
+    
+    function initiateBid(uint _itemId, uint _endTime) public {
+        require(msg.sender == itemToOwnerMapping[_itemId]);
+        itemToBiddingMap[_itemId] = biddingData(_endTime, 0, msg.sender, true);
+    }
+    
+    function flagUser(uint _itemId, address _flaggedUser) public {
+        require(msg.sender == itemToOwnerMapping[_itemId]);
+        require(itemToBiddingMap[_itemId].initialized == true);
+        itemToBiddingMap[_itemId].flaggedUsers[_flaggedUser] = true;
+    }
+    
+    //1. create function placeBid
+    
     function transferItem(uint _itemId, address _newOwner) public {
-        //require will return an error and exit if the condition is not met.
-        //here we are making sure whoever executes this function is the owner of the item 
-        //if he/she is not require will through error.
         require(msg.sender == itemToOwnerMapping[_itemId]);
         itemToOwnerMapping[_itemId] = _newOwner;
     }
