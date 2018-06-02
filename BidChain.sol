7e86cbc2f903113a669159a64567d1514a5c55c2
Avil Avate <avilavate123@gmail.com>
2018-06-02 20:10:24 +0530

    require will return an error and exit if the condition is not met. here we are making sure whoever executes this function is the owner of the item if he/she is not require will through error.

--- BidChain.sol
+++ BidChain.sol
@@ -1,8 +1,14 @@
-//1. create a contract BidChain
-//2. it should inherit AuctionHouse
 pragma solidity^0.4.18;
+
 import "./contracts.sol";
 
 contract BidChain is AuctionHouse {
     
+    function transferItem(uint _itemId, address _newOwner) public {
+        //require will return an error and exit if the condition is not met.
+        //here we are making sure whoever executes this function is the owner of the item 
+        //if he/she is not require will through error.
+        require(msg.sender == itemToOwnerMapping[_itemId]);
+        itemToOwnerMapping[_itemId] = _newOwner;
+    }
 }
