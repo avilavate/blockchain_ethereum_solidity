93b205c4c486532e8bb6c4c94ad1dc0e6c556e74
Avil Avate <avilavate123@gmail.com>
2018-06-02 18:25:41 +0530

    updated contract

--- contracts.sol
+++ contracts.sol
@@ -1,5 +1,6 @@
 pragma solidity^0.4.18;
 contract AuctionHouse {
+
     struct Item {
         string itemName;
         uint basePrice;
@@ -8,7 +9,12 @@ contract AuctionHouse {
 
     Item[] public items;
 
-    function createItem (string _name, uint _price, uint _itemId) public {
-        items.push(Item(_name,_price,_itemId));
+    function _generateItemId(string _str) private view returns (uint) {
+        uint hashOfName = uint(keccak256(_str));
+        return( hashOfName );
+    }
+
+    function _createItem(string _name, uint _price, uint _itemId) private {
+        items.push(Item(_name, _price, _itemId));
     }
 }
