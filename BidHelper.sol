7e71313c6652bafe8dd2d0c0ca05dfee8f598b95
A <avilavate@live.in>
2018-06-04 16:40:43 +0530

    getItemByOwner modified to return itemIdArray memory array and check validation to return items of senders only

--- BidHelper.sol
+++ BidHelper.sol
@@ -1,60 +1,40 @@
 pragma solidity^0.4.18;
 
-import "./contracts.sol";
+import "./BidChain.sol";
 
-contract BidChain is AuctionHouse {
+contract BidHelper is BidChain {
     
-    struct biddingData {
-        uint endTime;
-        uint highestBid;
-        address bidder;
-        bool initialized;
-        mapping (address => bool) flaggedUsers;
+    modifier itemOwner(uint _itemId) {
+        require(msg.sender == itemToOwnerMapping[_itemId]);
+        _;
     }
     
-    mapping (uint => biddingData) itemToBiddingMap;
-
-    event newBidPlaced(uint itemId, uint bidAmount, address bidder);
-    
-    function initiateBid(uint _itemId, uint _endTime) public {
-        require(msg.sender == itemToOwnerMapping[_itemId]);
-        itemToBiddingMap[_itemId] = biddingData(_endTime, 0, msg.sender, true);
-    }
-    
-    function flagUser(uint _itemId, address _flaggedUser) public {
-        require(msg.sender == itemToOwnerMapping[_itemId]);
+    function flagUser(uint _itemId, address _flaggedUser) public itemOwner(_itemId){
         require(itemToBiddingMap[_itemId].initialized == true);
         itemToBiddingMap[_itemId].flaggedUsers[_flaggedUser] = true;
     }
     
-    //1. add payable modifier to this function
-    //2. remove _bidAmount from parameter list
-    function placeBid(uint _itemId, uint _bidAmount) public {
-        require(now < itemToBiddingMap[_itemId].endTime);
-        
-        //3. change _bidAmount to msg.value 
-        require(_bidAmount > itemToBiddingMap[_itemId].highestBid);
-        itemToBiddingMap[_itemId].highestBid = _bidAmount;
-        itemToBiddingMap[_itemId].bidder = msg.sender;
-        
-        //4. change _bidAmount to msg.value
-        emit newBidPlaced(_itemId, _bidAmount, msg.sender);
+    function getItemDetail(uint _itemId) view public returns (string, uint, ItemState) {
+        uint counter = 0;
+        for(counter = 0; counter< items.length; counter++){
+            if(items[counter].itemId == _itemId){
+                return(items[counter].itemName, items[counter].basePrice, items[counter].state);
+            }
+        }
+        return("",0,ItemState.uninitialized);
     }
     
-    function transferItem(uint _itemId, address _newOwner) public {
-        require(msg.sender == itemToOwnerMapping[_itemId]);
-        itemToOwnerMapping[_itemId] = _newOwner;
-    }
-
     function getItemByOwner() view external returns (uint[]) {
         uint[] memory itemIdArray = new uint[](ownerToItemCount[msg.sender]);
-	    //1. define uint counter here
-		
-		//2. run a for loop from uint i = 0 to items.length
-		//3. inside the loop, check owner of itemId
-		//4. when owner is same as msg.sender, add itemId to the itemIdArray
-		
+        
+        uint counter = 0;
+        for(uint i=0;i<items.length;i++){
+            if(itemToOwnerMapping[items[i].itemId] == msg.sender){
+                itemIdArray[counter] = items[i].itemId;
+                counter++;
+            }
+        }
+        
         return itemIdArray;
     }
 }
-    
