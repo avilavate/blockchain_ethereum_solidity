22d00e459824343cca16329a8031ca557e4ee8cf
A <avilavate@live.in>
2018-06-04 10:30:29 +0530

    Added event newBidPlaced and raised it in placeBid function

--- BidHelper.sol
+++ BidHelper.sol
@@ -1,28 +1,49 @@
 pragma solidity^0.4.18;
 
-import "./BidChain.sol";
+import "./contracts.sol";
 
-contract BidHelper is BidChain {
+contract BidChain is AuctionHouse {
     
-    modifier itemOwner(uint _itemId) {
-        require(msg.sender == itemToOwnerMapping[_itemId]);
-        _;
+    struct biddingData {
+        uint endTime;
+        uint highestBid;
+        address bidder;
+        bool initialized;
+        mapping (address => bool) flaggedUsers;
     }
+    
+    mapping (uint => biddingData) itemToBiddingMap;
 
-    function flagUser(uint _itemId, address _flaggedUser) public itemOwner(_itemId) {
+    event newBidPlaced(uint itemId, uint bidAmount, address bidder);
+    
+    function initiateBid(uint _itemId, uint _endTime) public {
+        require(msg.sender == itemToOwnerMapping[_itemId]);
+        itemToBiddingMap[_itemId] = biddingData(_endTime, 0, msg.sender, true);
+    }
+    
+    function flagUser(uint _itemId, address _flaggedUser) public {
+        require(msg.sender == itemToOwnerMapping[_itemId]);
         require(itemToBiddingMap[_itemId].initialized == true);
         itemToBiddingMap[_itemId].flaggedUsers[_flaggedUser] = true;
     }
     
+    //1. add payable modifier to this function
+    //2. remove _bidAmount from parameter list
+    function placeBid(uint _itemId, uint _bidAmount) public {
+        require(now < itemToBiddingMap[_itemId].endTime);
+        
+        //3. change _bidAmount to msg.value 
+        require(_bidAmount > itemToBiddingMap[_itemId].highestBid);
+        itemToBiddingMap[_itemId].highestBid = _bidAmount;
+        itemToBiddingMap[_itemId].bidder = msg.sender;
+        
+        //4. change _bidAmount to msg.value
+        emit newBidPlaced(_itemId, _bidAmount, msg.sender);
+    }
     
-    //1. create a helper function, getItemDetail, which returns itemName, basePrice and state
-    function getItemDetail(uint _itemId) view public returns(string, uint, ItemState) {
-        uint counter = 0;
-        for(counter = 0; counter < items.length; counter++){
-            if(items[counter].itemId == _itemId){
-                return (items[counter].itemName, items[counter].basePrice, items[counter].state);
-            }
-        }
-        return ("",0,ItemState.uninitialized);
+    function transferItem(uint _itemId, address _newOwner) public {
+        require(msg.sender == itemToOwnerMapping[_itemId]);
+        itemToOwnerMapping[_itemId] = _newOwner;
     }
 }
+    
