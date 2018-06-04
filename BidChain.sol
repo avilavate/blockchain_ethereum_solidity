009498dc602420722ac025093d8d8e7b42ca1be4
A <avilavate@live.in>
2018-06-04 17:14:43 +0530

    Added itemOwner custom modifier to all the methods where item owner check is required and refactored the code!

--- BidChain.sol
+++ BidChain.sol
@@ -13,35 +13,56 @@ contract BidChain is AuctionHouse {
     }
     
     mapping (uint => biddingData) itemToBiddingMap;
+
+    event newBidPlaced(uint itemId, uint bidAmount, address bidder);
     
-    function initiateBid(uint _itemId, uint _endTime) public {
-        require(msg.sender == itemToOwnerMapping[_itemId]);
+    //1. add itemOwner modifier
+    function initiateBid(uint _itemId, uint _endTime) public itemOwner(_itemId) {
+        //2. refactor
+      
         itemToBiddingMap[_itemId] = biddingData(_endTime, 0, msg.sender, true);
     }
     
-    function returnBidAmount(address _sendee, uint _amount) internal {
-        _sendee.transfer(_amount);  
+    //3. add itemOwner modifier
+    function flagUser(uint _itemId, address _flaggedUser) public  itemOwner(_itemId) {
+        //4. refactor
+    
+        require(itemToBiddingMap[_itemId].initialized == true);
+        itemToBiddingMap[_itemId].flaggedUsers[_flaggedUser] = true;
     }
-
-    function placeBid(uint _itemId) public payable{
+    
+    function placeBid(uint _itemId) public payable {
         require(now < itemToBiddingMap[_itemId].endTime);
         require(msg.value > itemToBiddingMap[_itemId].highestBid);
-
+        
         returnBidAmount(itemToBiddingMap[_itemId].bidder, itemToBiddingMap[_itemId].highestBid);
         
         itemToBiddingMap[_itemId].highestBid = msg.value;
         itemToBiddingMap[_itemId].bidder = msg.sender;
+           
+        emit newBidPlaced(_itemId, msg.value, msg.sender);
     }
     
-    function transferItem(uint _itemId, address _newOwner) public {
-        require(msg.sender == itemToOwnerMapping[_itemId]);
-        //1. add another require condition to check that item should not be transferrable before it is end time
+    function returnBidAmount(address _sendee, uint _amount) internal {
+        _sendee.transfer(_amount);
+    }
+    
+    //5. add itemOwner modifier
+    function transferItem(uint _itemId, address _newOwner) public itemOwner(_itemId) {
+        //6. refactor
+        
         require(now > itemToBiddingMap[_itemId].endTime);
+        
         itemToOwnerMapping[_itemId] = _newOwner;
         ownerToItemCount[_newOwner]++;
         ownerToItemCount[msg.sender]--;
         
-        //2. transfer highestBid amount to the msg.sender as he is the owner of the item and he has transferred it to the new owner
         msg.sender.transfer(itemToBiddingMap[_itemId].highestBid);
+    }
+    
+    //We already moved this modifier here for you
+    modifier itemOwner(uint _itemId) {
+        require(msg.sender == itemToOwnerMapping[_itemId]);
+        _;
     }
 }
